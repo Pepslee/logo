0efe003ad78e152b3502d74083dc822ab50ac9ca
serhii <serhii@fountainhead-it.com>
2022-10-21 12:26:11 +0300

    keep counter button

--- lib/gridWidget.dart
+++ lib/gridWidget.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:logo/itemWidget.dart';
 
 class GridWidget extends StatelessWidget {
-  final List<String> items;
+  final int count;
 
-  const GridWidget({Key? key, required this.items}) : super(key: key);
+  const GridWidget({Key? key, required this.count}) : super(key: key);
   final double crossAxisCount = 2;
   final double axisSpacing = 10;
   final double gridMargin = 10;
@@ -22,28 +22,29 @@ class GridWidget extends StatelessWidget {
             slivers: [
               SliverToBoxAdapter(
                 child: ConstrainedBox(
-                  constraints: BoxConstraints(minHeight: minHeight),
+                  constraints: BoxConstraints(
+                      minHeight: minHeight, maxHeight: maxHeight),
                   child: Container(
                       margin: const EdgeInsets.all(30),
                       child: const Image(image: AssetImage('assets/logo.png'))),
                 ),
               ),
-              items.isNotEmpty ? SliverPadding(
-                padding: EdgeInsets.symmetric(horizontal: gridMargin),
+              SliverPadding(
+                padding: EdgeInsets.only(
+                    left: gridMargin, right: gridMargin, bottom: gridMargin),
                 sliver: SliverGrid(
                   delegate: SliverChildBuilderDelegate(
-                        (BuildContext context, int index) {
-                      return Item(itemName: items[index]);
+                    (BuildContext context, int index) {
+                      return Item(itemName: 'Item${index + 1}');
                     },
-                    childCount: items.length,
+                    childCount: count,
                   ),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: crossAxisCount.toInt(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
                       childAspectRatio: 2.0,
                       mainAxisSpacing: axisSpacing,
                       crossAxisSpacing: axisSpacing),
                 ),
-              ) : const SliverToBoxAdapter(
-                child: SizedBox.shrink(),
               )
             ],
           ),
@@ -54,19 +55,25 @@ class GridWidget extends StatelessWidget {
 
   double calcLogoHeight(double maxWidth, double maxHeight) {
     /// Calculation the grid cell with and height
-    double itemWidth = (maxWidth -
-        axisSpacing * (crossAxisCount - 1) -
-        gridMargin * 2) / crossAxisCount;
+    double itemWidth =
+        (maxWidth - axisSpacing * (crossAxisCount - 1) - gridMargin * 2) /
+            crossAxisCount;
     double itemHeight = itemWidth / 2;
+
     /// Calculation rows count
-    int rowCount = (items.length / crossAxisCount).ceil();
+    int rowCount = (count / crossAxisCount).ceil();
+    /// When count of rows is zero, however GridView has height of 1 row.
+    rowCount = rowCount == 0 ? 1 : rowCount;
+
     /// Calculation of one cell height to show from bottom
-    double bottomShift = itemHeight * rowCount +
-        axisSpacing * (rowCount - 1) + gridMargin;
+    double bottomShift =
+        itemHeight * rowCount + axisSpacing * (rowCount - 1) + gridMargin;
+
     /// Calculation size of logo widget
     double minHeight = maxHeight - bottomShift;
+
     /// Avoid negative height if bottomShift is to big
-    minHeight = minHeight < 0 ? 0: minHeight;
+    minHeight = minHeight < 0 ? 0 : minHeight;
     return minHeight;
   }
 }
